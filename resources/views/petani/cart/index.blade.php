e6b90873871b532416e10586bdb0ee80e0b3b1a5
yogipr <yogipratamat10@gmail.com>
2021-10-14 13:40:50 +0800

    Update tampilan Cart

--- resources/views/petani/cart/index.blade.php
+++ resources/views/petani/cart/index.blade.php
@@ -4,188 +4,17 @@
 
 @section('asset')
 
-    <script src="/assets/js/app.js"></script>
+    <!-- Theme JS files -->
+    <script src="/global_assets/js/plugins/tables/datatables/datatables.min.js"></script>
+    <script src="/global_assets/js/plugins/forms/selects/select2.min.js"></script>
+    <script src="/global_assets/js/plugins/tables/datatables/extensions/jszip/jszip.min.js"></script>
+    <script src="/global_assets/js/plugins/tables/datatables/extensions/pdfmake/pdfmake.min.js"></script>
+    <script src="/global_assets/js/plugins/tables/datatables/extensions/pdfmake/vfs_fonts.min.js"></script>
+    <script src="/global_assets/js/plugins/tables/datatables/extensions/buttons.min.js"></script>
 
-    <style>
-        body {
-            background: #ddd;
-            min-height: 100vh;
-            vertical-align: middle;
-            display: flex;
-            font-family: sans-serif;
-            font-size: 0.8rem;
-            font-weight: bold
-        }
-
-        .title {
-            margin-bottom: 5vh
-        }
-
-        .card {
-            margin: auto;
-            max-width: 950px;
-            width: 90%;
-            box-shadow: 0 6px 20px 0 rgba(0, 0, 0, 0.19);
-            border-radius: 1rem;
-            border: transparent
-        }
-
-        @media(max-width:767px) {
-            .card {
-                margin: 3vh auto
-            }
-        }
-
-        .cart {
-            background-color: #fff;
-            padding: 4vh 5vh;
-            border-bottom-left-radius: 1rem;
-            border-top-left-radius: 1rem
-        }
-
-        @media(max-width:767px) {
-            .cart {
-                padding: 4vh;
-                border-bottom-left-radius: unset;
-                border-top-right-radius: 1rem
-            }
-        }
-
-        .summary {
-            background-color: #ddd;
-            border-top-right-radius: 1rem;
-            border-bottom-right-radius: 1rem;
-            padding: 4vh;
-            color: rgb(65, 65, 65)
-        }
-
-        @media(max-width:767px) {
-            .summary {
-                border-top-right-radius: unset;
-                border-bottom-left-radius: 1rem
-            }
-        }
-
-        .summary .col-2 {
-            padding: 0
-        }
-
-        .summary .col-10 {
-            padding: 0
-        }
-
-        .row {
-            margin: 0
-        }
-
-        .title b {
-            font-size: 1.5rem
-        }
-
-        .main {
-            margin: 0;
-            padding: 2vh 0;
-            width: 100%
-        }
-
-        .col-2,
-        .col {
-            padding: 0 1vh
-        }
-
-        a {
-            padding: 0 1vh
-        }
-
-        .close {
-            margin-left: auto;
-            font-size: 0.7rem
-        }
-
-        img {
-            width: 3.5rem
-        }
-
-        .back-to-shop {
-            margin-top: 4.5rem
-        }
-
-        h5 {
-            margin-top: 4vh
-        }
-
-        hr {
-            margin-top: 1.25rem
-        }
-
-        form {
-            padding: 2vh 0
-        }
-
-        select {
-            border: 1px solid rgba(0, 0, 0, 0.137);
-            padding: 1.5vh 1vh;
-            margin-bottom: 4vh;
-            outline: none;
-            width: 100%;
-            background-color: rgb(247, 247, 247)
-        }
-
-        input {
-            border: 1px solid rgba(0, 0, 0, 0.137);
-            padding: 1vh;
-            margin-bottom: 4vh;
-            outline: none;
-            width: 100%;
-            background-color: rgb(247, 247, 247)
-        }
-
-        input:focus::-webkit-input-placeholder {
-            color: transparent
-        }
-
-        .btn {
-            background-color: #000;
-            border-color: #000;
-            color: white;
-            width: 100%;
-            font-size: 0.7rem;
-            margin-top: 4vh;
-            padding: 1vh;
-            border-radius: 0
-        }
-
-        .btn:focus {
-            box-shadow: none;
-            outline: none;
-            box-shadow: none;
-            color: white;
-            -webkit-box-shadow: none;
-            -webkit-user-select: none;
-            transition: none
-        }
-
-        .btn:hover {
-            color: white
-        }
-
-        a {
-            color: black
-        }
-
-        a:hover {
-            color: black;
-            text-decoration: none
-        }
-
-        #code {
-            background-image: linear-gradient(to left, rgba(255, 255, 255, 0.253), rgba(255, 255, 255, 0.185)), url("https://img.icons8.com/small/16/000000/long-arrow-right.png");
-            background-repeat: no-repeat;
-            background-position-x: 95%;
-            background-position-y: center
-        }
-
-    </style>
+    <script src="assets/js/app.js"></script>
+    <script src="/global_assets/js/demo_pages/ecommerce_orders_history.js"></script>
+    <!-- /theme JS files -->
 
 @endsection
 
@@ -208,71 +37,79 @@
                 <span class="font-weight-semibold">{{ Session('success') }} </span>
             </div>
         @endif
-        <div class="card">
+        <div style="border-radius:1rem" class="card shadow">
             <div class="row">
-                <div class="col-md-8 cart">
-                    <div class="title">
-                        <div class="row">
-                            <div class="col">
-                                <h4><b>Keranjang Pesanan</b></h4>
-                            </div>
-                            <div class="col align-self-center text-right text-muted">{{ $total }} items</div>
-                        </div>
+                <div class="col-sm-8">
+                    <div class="card-header bg-transparent header-elements-inline justify-content-center">
+                        <h2 class="card-title"><b>Keranjang Pesanan</b></h2>
                     </div>
-                    <div class="row border-top border-bottom">
-                        <div class="row main align-items-center">
-                            <div class="col-2"></div>
-                            <div class="col">
-                                Nama Produk
-                            </div>
-                            <div class="col">Jumlah</div>
-
-                            <div class="col">Harga Satuan</div>
-                            <div class="col">Total Harga</div>
-                        </div>
+                    <div class="table-responsive">
+                        <table class="table text-nowrap">
+                            <thead>
+                                <tr>
+                                    <th colspan="2">Nama Produk</th>
+                                    <th>Jumlah</th>
+                                    <th>Harga Stuan</th>
+                                    <th>Total Harga</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr class="table-active">
+                                    <td colspan="7" class="font-weight-semibold">{{ $total }} items</td>
+                                    <td class="text-right">
+                                        <span class="badge bg-secondary badge-pill">{{ $total }} items</span>
+                                    </td>
+                                </tr>
+                                @if ($carts)
+                                    @forelse ($carts as $cart)
+                                        <tr>
+                                            <td class="pr-0" style="width: 45px;">
+                                                <a href="#">
+                                                    <img src="{{ asset('/storage' . $cart['image']) }}" height="60"
+                                                        alt="">
+                                                </a>
+                                            </td>
+                                            <td>
+                                                <a href="#" class="font-weight-semibold">{{ $cart['name'] }}</a>
+                                            </td>
+                                            <td>{{ $cart['qty'] }}</td>
+                                            <td>Rp. {{ number_format($cart['price'], 2, ',', '.') }}</td>
+                                            <td>
+                                                Rp.{{ number_format($cart['price'] * $cart['qty'], 2, ',', '.') }}
+                                                <a href="{{ route('petani.cart.deleteFromCart', [$cart['id']]) }}"><span
+                                                        class="close">&#10005;</span></a>
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                @else
+                                    <div class="text-center mt-5 mb-3">
+                                        <h6><b>Data Kosong</b></h6>
+                                    </div>
+                                @endif
+                            </tbody>
+                        </table>
                     </div>
-                    @if ($carts)
-                        @forelse ($carts as $cart)
-                            <div class="row border-top border-bottom">
-                                <div class="row main align-items-center">
-                                    <div class="col-2"><img class="img-fluid"
-                                            src="{{ asset('/storage' . $cart['image']) }}"></div>
-                                    <div class="col">
-                                        <div class="row text-muted">{{ $cart['name'] }}</div>
-                                    </div>
-                                    <div class="col">{{ $cart['qty'] }}</div>
-
-                                    <div class="col">Rp. {{ number_format($cart['price'], 2, ',', '.') }}
-                                    </div>
-
-                                    <div class="col">Rp.
-                                        {{ number_format($cart['price'] * $cart['qty'], 2, ',', '.') }}
-                                        <a href="{{ route('petani.cart.deleteFromCart', [$cart['id']]) }}">
-                                            <span class="close">&#10005;</span></a>
-                                    </div>
-                                </div>
-                            </div>
-                        @endforeach
-                    @else
-                        <div class="text-center mt-2">Data kosong</div>
-                    @endif
-                    <div class="back-to-shop"><a href="{{ route('petani.product.index') }}">&leftarrow;</a><span
-                            class="text-muted">Back to shop</span></div>
                 </div>
-                <div class="col-md-4 summary">
-                    <div>
-                        <h5><b>Summary</b></h5>
+                <div style="border-top-right-radius: 1rem" class="col-sm-4 bg-grey-300">
+                    <div class="card-header mt-2 bg-transparent header-elements-inline justify-content-center">
+                        <h5 class="card-title"><b>Sumary</b></h5>
                     </div>
-                    <hr>
-                    <div class="row">
-                        <div class="col" style="padding-left:0;">ITEMS</div>
-                        <div class="col text-right">{{ $total }}</div>
-                    </div>
-                    <div class="row" style="border-top: 1px solid rgba(0,0,0,.1); padding: 2vh 0;">
-                        <div class="col" style="padding-left:0;">TOTAL PRICE</div>
-                        <div class="col text-right">Rp. {{ number_format($totalPrice, 2, ',', '.') }}</div>
-                    </div> <a href="{{ route('petani.cart.checkout') }}"><button
-                            class="btn">CHECKOUT</button></a>
+                    <table class="table text-nowrap">
+                        <tr>
+                            <th>ITEMS </th>
+                            <th>{{ $total }}</th>
+                        </tr>
+                        <tr>
+                            <th>Total Harga </th>
+                            <th>Rp. {{ number_format($totalPrice, 2, ',', '.') }}</th>
+                        </tr>
+                        <tr class="col text-center">
+                            <th colspan="2">
+                                <a class="btn btn-dark mt-4 pl-3 pr-3"
+                                    href="{{ route('petani.cart.checkout') }}">CHECKOUT</a>
+                            </th>
+                        </tr>
+                    </table>
                 </div>
             </div>
         </div>
